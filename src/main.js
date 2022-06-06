b26714d9a666726eefbd7f2bcbb398dfaf65e531
yesswssdlh <3278195170@qq.com>
2022-06-06 14:54:37 +0800

    优化

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import i18n from '@/locales/index';
+import i18n from '@/locales';
 import App from './App.vue';
 import router from './router';
 import store from './store';
